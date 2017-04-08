4acaac6e559fa17f55b8ce3cd5fd85e5d36824b4
AlexPThomas <apthomas1000@gmail.com>
2017-04-08 23:06:04 +1200

    initial commit got angular 2 and nodejs working

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/systemjs.config.js', express.static(__dirname + '/public/javascripts/systemjs.config.js'));
+app.use('/shim.min.js', express.static(__dirname + '/node_modules/core-js/client/shim.min.js'));
+app.use('/zone.js', express.static(__dirname + '/node_modules/zone.js/dist/zone.js'));
+app.use('/system.src.js', express.static(__dirname + '/node_modules/systemjs/dist/system.src.js'));
+app.use('/angular', express.static(__dirname + '/views/app'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
