5e6b5e3dc6eacc7fbd265b34c5fc1b59601ab7c0
AlexPThomas <apthomas1000@gmail.com>
2017-05-07 00:16:56 +1200

    added a ripple effect to the list item cards

--- views/app/app.module.ts
+++ views/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 import {MdButtonModule, MdCheckboxModule, MdCardModule,MdProgressSpinnerModule,MdToolbarModule,MdIconModule,
-        MdListModule} from '@angular/material';
+        MdListModule, MdRippleModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent }  from './app.component.js';
 import {LoaderComponent} from './loader.component.js'
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpModule, JsonpModule,
                   MdButtonModule, MdCheckboxModule,MdCardModule, BrowserAnimationsModule,MdProgressSpinnerModule,
-                  MdToolbarModule,MdIconModule, MdListModule],
+                  MdToolbarModule,MdIconModule, MdListModule,MdRippleModule],
   declarations: [ AppComponent,LoaderComponent ],
   bootstrap:    [ AppComponent ]
 })
